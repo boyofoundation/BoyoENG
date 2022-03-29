ae3e0e8aaa4ca7bdbf7549b0acef1025db299dab
killkli <dchensterebay@gmail.com>
2022-03-29 11:03:21 +0800

    deploy

---
+++ precache-manifest.382828d24e03d7549374cc1d5114d89b.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f964ef27e1fc0c379dd983761d7ab1f7",
+    "url": "/BoyoENG/.htaccess"
+  },
+  {
+    "revision": "f964ef27e1fc0c379dd983761d7ab1f7",
+    "url": "/BoyoENG/.htaccess_AMS.txt"
+  },
+  {
+    "revision": "bdcc71b38edb7404285682ae705b9417",
+    "url": "/BoyoENG/.htaccess_DO.txt"
+  },
+  {
+    "revision": "0c1710db3903c590e0d2",
+    "url": "/BoyoENG/css/app.eb553c83.css"
+  },
+  {
+    "revision": "6229265f8cb133bb536b",
+    "url": "/BoyoENG/css/chunk-vendors.facf7b87.css"
+  },
+  {
+    "revision": "adfdcdea5c03ac9426f474cc1ef50d4e",
+    "url": "/BoyoENG/favicon.ico"
+  },
+  {
+    "revision": "7d9421045bcabc1e90e1458e72c6a5cb",
+    "url": "/BoyoENG/icon.png"
+  },
+  {
+    "revision": "985ba0faefe64ca3bb0c665fb4563a8d",
+    "url": "/BoyoENG/img/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "88280466f33992b17dce5305261e018e",
+    "url": "/BoyoENG/img/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "f6e728eee5b0a926a91db5eb27800978",
+    "url": "/BoyoENG/img/icons/android-chrome-maskable-192x192.png"
+  },
+  {
+    "revision": "2d81fe480aa9fc0f6067bc0f94670452",
+    "url": "/BoyoENG/img/icons/android-chrome-maskable-512x512.png"
+  },
+  {
+    "revision": "b7647d12a3f99b3c079c4d9bd53593d8",
+    "url": "/BoyoENG/img/icons/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "3b9c26616da70b1555dd0893abdc38b9",
+    "url": "/BoyoENG/img/icons/apple-touch-icon-152x152.png"
+  },
+  {
+    "revision": "a341f52a7499f028c39bdef9e1fd0353",
+    "url": "/BoyoENG/img/icons/apple-touch-icon-180x180.png"
+  },
+  {
+    "revision": "d67e842e7928a3f3d59a80da39deb762",
+    "url": "/BoyoENG/img/icons/apple-touch-icon-60x60.png"
+  },
+  {
+    "revision": "08a23740ef986f79ff9049514b1ae2cc",
+    "url": "/BoyoENG/img/icons/apple-touch-icon-76x76.png"
+  },
+  {
+    "revision": "acd013ff94f3589134558761fb581e98",
+    "url": "/BoyoENG/img/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "fcc448503465c5e06a1a874c614d1909",
+    "url": "/BoyoENG/img/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "769f97066e6c428cb57a77657dacc22d",
+    "url": "/BoyoENG/img/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "a2ab0d8cca40f4f86f3f4ec393c3507b",
+    "url": "/BoyoENG/img/icons/msapplication-icon-144x144.png"
+  },
+  {
+    "revision": "6a9d3acd69106db00ca794a286e0602a",
+    "url": "/BoyoENG/img/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "e9c7d5cbf5e30860c62675dceff43ba2",
+    "url": "/BoyoENG/img/icons/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "0d85bfddff27b4eb7d72c4a900864eaa",
+    "url": "/BoyoENG/index.html"
+  },
+  {
+    "revision": "0c1710db3903c590e0d2",
+    "url": "/BoyoENG/js/app.12c0cd80.js"
+  },
+  {
+    "revision": "6229265f8cb133bb536b",
+    "url": "/BoyoENG/js/chunk-vendors.85dfdfec.js"
+  },
+  {
+    "revision": "18efb8c2d68ca8806140932c18caec32",
+    "url": "/BoyoENG/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/BoyoENG/robots.txt"
+  }
+]);
